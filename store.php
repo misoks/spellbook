014c249516aae5f324cab8069680bb0a99ae869c
Cathy Fisher <cathy.a.fisher@gmail.com>
2017-04-02 11:17:57 -0500

    Fix encoding issues

--- store.php
+++ store.php
@@ -10,7 +10,7 @@
 	$newSpell["name"] = $_POST["spellName"];
 	$newSpell["casting_time"] = $_POST["time"];
 	$newSpell["components"] = $_POST["components"];
-	$newSpell["description"] = $_POST["description"];
+	$newSpell["description"] = utf8_encode($_POST["description"]);
 	$newSpell["duration"] = $_POST["duration"];
 	$newSpell["level"] = $_POST["level"];
 	$newSpell["range"] = $_POST["range"];
@@ -22,22 +22,53 @@
 	$myFile = "spells.json";
 	$jsonString = file_get_contents($myFile);
 	$spellArray = json_decode($jsonString, true);
+	if (!$spellArray["spells"]) {
+		echo "Error json decoding string";
+		exit;
+	}
 
 	array_push($spellArray["spells"], $newSpell);
 
 	$modifiedSpellString = json_encode($spellArray);
-	$error = json_last_error();
-	if (!$error == JSON_ERROR_NONE) {
+	if (!$modifiedSpellString) {
+		echo "Error json encoding array";
+		exit;
+	}
+
+	switch (json_last_error()) {
+        case JSON_ERROR_NONE:
+            echo ' - No errors';
+        break;
+        case JSON_ERROR_DEPTH:
+            echo ' - Maximum stack depth exceeded';
+        break;
+        case JSON_ERROR_STATE_MISMATCH:
+            echo ' - Underflow or the modes mismatch';
+        break;
+        case JSON_ERROR_CTRL_CHAR:
+            echo ' - Unexpected control character found';
+        break;
+        case JSON_ERROR_SYNTAX:
+            echo ' - Syntax error, malformed JSON';
+        break;
+        case JSON_ERROR_UTF8:
+            echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+        break;
+        default:
+            echo ' - Unknown error';
+        break;
+    }
+	if ($error == JSON_ERROR_NONE) {
 		$fh = fopen($myFile, 'w') or die("Can't open file");
 		$success = file_put_contents($myFile,$modifiedSpellString);
 		echo $success;
 		fclose($fh);
+	    echo "<META HTTP-EQUIV='Refresh' Content='0; URL=index.html'>";    
+	   
 	}
 
-
 	unset($_POST);
+	exit; 
 
-    echo "<META HTTP-EQUIV='Refresh' Content='0; URL=index.html'>";    
-    exit; 
 	
 ?>
